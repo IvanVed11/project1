58601452d1508eb3cd0fb51eebba23b069f71cb6
Ivan <ivan.ved2020@mail.ru>
2023-11-18 09:59:25 +0300

    1.53

--- 1.1.py
+++ 1.1.py
@@ -1,1 +1,17 @@
 
+n = int(input())
+temperature = float(input())
+maximum = temperature
+temperature = float(input())
+if maximum < temperature:
+    minimum = temperature
+else:
+    minimum = maximum
+    maximum = temperature
+for temperature in range(2, n + 1):
+    temperature = float(input())
+    if maximum < temperature:
+        maximum = temperature
+    if temperature < minimum:
+        minimum = temperature
+print(maximum - minimum)
